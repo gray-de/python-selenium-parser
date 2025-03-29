0a79f22d7c56a31d93d5e845b22b2ac6ffc74b5c
Andrey in  Digital <andtelephonezte@gmail.com>
2025-03-29 18:25:46 +0300

    xml norm

--- parser/parser.py
+++ parser/parser.py
@@ -11,12 +11,12 @@ import time
 import random
 import re
 
-
-options = webdriver.ChromeOptions()
-options.page_load_strategy = "eager"
+options = webdriver.ChromeOptions()  # создаем webdriver
+options.page_load_strategy = "eager"  # добавляем настройку в драйвер, чтобы не дожидаться полной подгрузки сайта(сайт долго грузиться, поэтому selenium простаивает)
 options.add_argument("--ignore-certificate-errors --allow-insecure-localhost")
-service = Service(executable_path=ChromeDriverManager().install())
-driver = webdriver.Chrome(service=service, options=options)
+service = Service(
+    executable_path=ChromeDriverManager().install())  # Service для инициализации, он устанавливает, создаёт и удаляет драйвер
+driver = webdriver.Chrome(service=service, options=options)  # Chrome будет управляться service'ом
 driver.command_executor.set_timeout(1000)
 
 
@@ -28,77 +28,94 @@ def add_empty_dates(text):
     for label, date_str in dates:
         date_dict[label.lower()] = date_str.strip() if date_str else ""
 
-    return (
-        date_dict.get("submission", ""),
-        date_dict.get("revised", ""),
-        date_dict.get("accepted", ""),
-        date_dict.get("published", "")
-    )
+    submission_date = date_dict.get('submission', '')
+    revised_date = date_dict.get('revised', '')
+    accepted_date = date_dict.get('accepted', '')
+    published_date = date_dict.get('published', '')
+
+    return submission_date, revised_date, accepted_date, published_date
 
 
 root = ET.Element("articles")
 k = 0
+for num in range(1, 107):  # перебираем все страницы с статьями
+    driver.get(
+        f"https://www.mdpi.com/search?sort=pubdate&page_no={num}&page_count=50&year_from=1996&year_to=2025&q=medicine&view=default")  # открываем одну страницу с статьями
+    print(
+        f"https://www.mdpi.com/search?sort=pubdate&page_no={num}&page_count=50&year_from=1996&year_to=2025&q=medicine&view=default")
+    time.sleep(3)
+    links = driver.find_elements(By.CSS_SELECTOR, ".title-link")  # находим объекты заголовков статей
+    links_list = []
+    for i in links:
+        links_list.append(i.get_attribute("href"))  # собираем в один список ссылке на статьи с этой страницы
+    print(links_list)
+    for i in links_list:
+        try:
+            driver.get(i)  # открываем одну статью
+            print(i)
+            k += 1
+        except ReadTimeoutError:
+            driver.refresh()
+        time.sleep(random.randint(3, 6))
+        try:
+            element = WebDriverWait(driver, 30).until(
+                EC.presence_of_element_located((By.CSS_SELECTOR,
+                                                ".title.hypothesis_container")))  # добавляем ожидание в 10 секунд на появление на странице объекта, если он не успевает появится, то выводиться ошибка
+        except TimeoutException:
+            print("Time is out, skipping to the next text")
+            break
 
-with open("articles1.xml", "wb") as f:
-    f.write(b'<?xml version="1.0" encoding="utf-8"?>\n<articles>\n')
+        current_url = driver.current_url  # переменная с ссылкой на страницу
+        title = driver.find_element(By.CSS_SELECTOR,
+                                    ".title.hypothesis_container").text  # по css селектору находим заголовок статьи
+        date = driver.find_element(By.CSS_SELECTOR, ".pubhistory").text  # по css селектору находим даты статьи
 
-    for num in range(1, 107):
-        driver.get(
-            f"https://www.mdpi.com/search?sort=pubdate&page_no={num}&page_count=50&year_from=1996&year_to=2025&q=medicine&view=default")
-        print(f"Processing page {num}")
-        time.sleep(3)
+        # dates_list = date.split('/')
+        # print(dates_list)
+        # res_dates_list = []
+        # while len(dates_list) < 4:
+        #     dates_list.append('')
+        # for i in dates_list:
+        #     res_dates_list.append(i[i.find(':')+2:])
 
-        links = [i.get_attribute("href") for i in driver.find_elements(By.CSS_SELECTOR, ".title-link")]
+        submission, revised, accepted, published = add_empty_dates(date)
+        print(submission, revised, accepted, published)
+        authors_list = driver.find_elements(By.CSS_SELECTOR, ".profile-card-drop")  # по css селектору находим авторов
+        authors = []
+        for i in authors_list:
+            authors.append(i.text)  # добавляем авторов в отдельный список
 
-        for link in links:
-            try:
-                driver.get(link)
-                print(link)
-                k += 1
-            except ReadTimeoutError:
-                driver.refresh()
+        keywords = driver.find_elements(By.CSS_SELECTOR, ".html-gwd-group")  # находим ключевые слова статьи
+        keywords_list = []
+        for i in keywords:
+            keywords_list.append(i.text)
+        if len(keywords_list) == 0:
+            res_keywords = []
+        else:
+            str_keywords = keywords_list[0]
+            print(keywords_list)
+            res_keywords = str_keywords[10:].split("; ")
+            print(res_keywords)
+        print(k)
 
-            time.sleep(random.randint(3, 6))
+        # Добавляем статью в XML
+        article = ET.SubElement(root, "article")
+        ET.SubElement(article, "title").text = title
+        ET.SubElement(article, "url").text = current_url
+        ET.SubElement(article, "submission_date").text = submission
+        ET.SubElement(article, "revised_date").text = revised
+        ET.SubElement(article, "accepted_date").text = accepted
+        ET.SubElement(article, "published_date").text = published
 
-            try:
-                WebDriverWait(driver, 30).until(
-                    EC.presence_of_element_located((By.CSS_SELECTOR, ".title.hypothesis_container"))
-                )
-            except TimeoutException:
-                print("Timeout, skipping...")
-                continue
+        authors_elem = ET.SubElement(article, "authors")
+        for author in authors:
+            ET.SubElement(authors_elem, "author").text = author
 
-            current_url = driver.current_url
-            title = driver.find_element(By.CSS_SELECTOR, ".title.hypothesis_container").text
-            date = driver.find_element(By.CSS_SELECTOR, ".pubhistory").text
-            submission, revised, accepted, published = add_empty_dates(date)
+        keywords_elem = ET.SubElement(article, "keywords")
+        for keyword in res_keywords:
+            ET.SubElement(keywords_elem, "keyword").text = keyword
 
-            authors = [i.text for i in driver.find_elements(By.CSS_SELECTOR, ".profile-card-drop")]
-
-            keywords = driver.find_elements(By.CSS_SELECTOR, ".html-gwd-group")
-            keywords_list = keywords[0].text[10:].split("; ") if keywords else []
-
-            # Запись XML непосредственно в файл
-            f.write(f'  <article>\n'.encode())
-            f.write(f'    <title>{title}</title>\n'.encode())
-            f.write(f'    <url>{current_url}</url>\n'.encode())
-            f.write(f'    <submission_date>{submission}</submission_date>\n'.encode())
-            f.write(f'    <revised_date>{revised}</revised_date>\n'.encode())
-            f.write(f'    <accepted_date>{accepted}</accepted_date>\n'.encode())
-            f.write(f'    <published_date>{published}</published_date>\n'.encode())
-
-            f.write(f'    <authors>\n'.encode())
-            for author in authors:
-                f.write(f'      <author>{author}</author>\n'.encode())
-            f.write(f'    </authors>\n'.encode())
-
-            f.write(f'    <keywords>\n'.encode())
-            for keyword in keywords_list:
-                f.write(f'      <keyword>{keyword}</keyword>\n'.encode())
-            f.write(f'    </keywords>\n'.encode())
-
-            f.write(f'  </article>\n'.encode())
-
-    f.write(b'</articles>')
-
-driver.quit()
+        # Записываем XML в файл после обработки каждой статьи
+        tree = ET.ElementTree(root)
+        with open("articles1.xml", "wb") as f:
+            tree.write(f, encoding="utf-8", xml_declaration=True)
